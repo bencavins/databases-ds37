6c02cd712e9c1a2349ece895a8128a74d1abf80c
Ben Cavins <bencavins@gmail.com>
2022-04-21 15:40:10 -0400

    add some queries

--- sprint_challenge/demo_data.py
+++ sprint_challenge/demo_data.py
@@ -29,10 +29,7 @@ FROM demo
 WHERE x >= 5 AND y >= 5;
 """
 
-COUNT_DISTINCT_Y = """
-SELECT COUNT(DISTINCT y)
-FROM demo;
-"""
+COUNT_DISTINCT_Y = "SELECT COUNT(DISTINCT y) FROM demo;"
 
 
 def create_sqlite_connection(db_name='demo_data.sqlite3'):
@@ -53,6 +50,30 @@ def execute_data_update(connection, query):
 
 
 sl_conn = create_sqlite_connection()
+execute_data_update(sl_conn, CREATE_TABLE)
+# execute_data_update(sl_conn, INSERT_DATA)
 row_count = execute_query(sl_conn, COUNT_ROWS)
 xy_at_least_5 = execute_query(sl_conn, COUNT_XY_AT_LEAST_5)
 unique_y = execute_query(sl_conn, COUNT_DISTINCT_Y)
+
+
+expensive_items = """
+SELECT *
+FROM Product
+ORDER BY UnitPrice DESC
+LIMIT 10;
+"""
+
+avg_hire_age = """
+SELECT AVG(HireDate - BirthDate)
+FROM Employee
+"""
+
+ten_most_expensive = """
+SELECT p.ProductName, p.UnitPrice, s.CompanyName
+FROM Product as p
+JOIN Supplier as s
+	ON s.Id = p.SupplierId
+ORDER BY p.UnitPrice DESC
+LIMIT 10;
+"""
